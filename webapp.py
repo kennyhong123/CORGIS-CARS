247041f65f2bded7e46a3bca88f06e93d3d60464
kennyhong123 <31251440+kennyhong123@users.noreply.github.com>
2018-01-15 13:30:18 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -9,12 +9,66 @@ def render_main():
     return render_template('home.html')
 
 @app.route("/horsepower")
-def render_horsepower():
-    with open('cars.json') as cars_data:
+def render_highest_horsepower():
+    with open('car.json') as cars_data:
         cars = json.load(cars_data)
-    longestData = get_longest_dam(cars)
-    return render_template('horsepower.html', length = longestData[1])
+    highestData = get_highest_horsespower(cars)
+    return render_template('horsepower.html', highest = highestData[1])
+@app.route("/dataByDam")
+def render_data_by_dam():
+    with open('hydropower.json') as dams_data:
+        dams = json.load(dams_data)
+    if 'dam' in request.args:
+        d = get_dam_data(dams, request.args['dam'])
+        return render_template('data-by-dam.html', options = get_dam_options(dams), name = d["Identity"]["Name"], year = d["Identity"]["Project"]["Year"], state = d["Location"]["State"], length = d["Dimensions"]["Crest Length"], height = d["Dimensions"]["Structural Height"])
+    return render_template('data-by-dam.html', options = get_dam_options(dams))
 
+@app.route("/damsPerState")
+def render_dams_per_state():
+    with open('hydropower.json') as dams_data:
+        dams = json.load(dams_data)
+    if 'state' in request.args:
+        return render_template('dams-per-state.html', options = get_state_options(dams), numDams = get_dams_per_state(dams, request.args['state']), state = request.args['state'])
+    return render_template('dams-per-state.html', options = get_state_options(dams))
+
+def get_dam_options(dams):
+    names = []
+    options = ""
+    for d in dams:
+        if d["Identity"]["Name"] not in dams:
+            names.append(d["Identity"]["Name"])
+            options += Markup("<option value=\"" + d["Identity"]["Name"] + "\">" + d["Identity"]["Name"] + "</option>")
+    return options
+
+def get_state_options(dams):
+    states = []
+    options = ""
+    for d in dams:
+        if d["Location"]["State"] not in states:
+            states.append(d["Location"]["State"])
+            options += Markup("<option value=\"" + d["Location"]["State"] + "\">" + d["Location"]["State"] + "</option>")
+    return options
+
+def get_dam_data(dams, selected_dam):
+    for d in dams:
+        if d["Identity"]["Name"] == selected_dam:
+            return d
+
+def get_dams_per_state(dams, selected_state):
+    numDams = 0
+    for d in dams:
+        if d["Location"]["State"] == selected_state:
+            numDams += 1
+    return numDams
+
+def get_highest_horsepower(cars):
+    highest = 0
+    highestHorsepower = ""
+    for d in cars:
+        if d["Engine Information"]["Engine Statistics"]["Horsepower"] > length:
+            highest = d["Engine Information"]["Engine Statistics"]["Horsepower"]
+            highestHorsepower = d["Engine Information"]["Engine Statistics"]["Horsepower"]
+    return [highestHorsepower, highest]
 
 
 if __name__=="__main__":

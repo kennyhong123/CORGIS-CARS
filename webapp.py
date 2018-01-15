af12b027c0ac8c913d2762f0e26d44e1db02ec89
kennyhong123 <31251440+kennyhong123@users.noreply.github.com>
2018-01-15 13:52:51 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,9 +8,31 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 def render_main():
     return render_template('home.html')
 
-@app.route("/horsepower")
-def render_horsepower():
-    return render_template('horsepower.html')
+@app.route("/laregstCar")
+def render_largest_car():
+    with open('cars.json') as cars_data:
+        cars = json.load(cars_data)
+    longestData = get_longest_car(cars)
+    tallestData = get_tallest_car(cars)
+    return render_template('horsepower.html',longest = longestData[0], length = longestData[1], tallest = tallestData[0], height = tallestData[1])
+ 
+def get_longest_car(cars)
+    length = 0
+    longestCar = ""
+    for c in cars:
+        if c["Dimensions"]["Length"] > length:
+            length = c["Dimensions"]["Length"]
+            longestCar = c["Identification"]["Make"]
+    return [longestCar, length]
+      
+def gget_tallest_car(cars):
+    height = 0
+    tallestCar = ""
+    for c in cars:
+        if c["Dimensions"]["Height"] > height:
+            height = c["Dimensions"]["Height"]
+            tallestCar = c["Identification"]["Make"]
+    return [tallestCar, height]
 
 if __name__=="__main__":
     app.run(debug=False, port=54321)

1637cb639e4be131d5b9d1600de98248a2fdd0c6
kennyhong123 <31251440+kennyhong123@users.noreply.github.com>
2018-01-15 13:59:07 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,14 +8,22 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 def render_main():
     return render_template('home.html')
 
-@app.route("/laregstCar")
-def render_largest_car():
+@app.route("/laregstCars")
+def render_largest_cars():
     with open('cars.json') as cars_data:
         cars = json.load(cars_data)
     longestData = get_longest_car(cars)
     tallestData = get_tallest_car(cars)
-    return render_template('horsepower.html',longest = longestData[0], length = longestData[1], tallest = tallestData[0], height = tallestData[1])
+    return render_template('largest-cars.html',longest = longestData[0], length = longestData[1], tallest = tallestData[0], height = tallestData[1])
  
+def get_car_options(cars):
+    names = []
+    options = ""
+    for c in cars:
+        if c["Identification"]["Make"] not in cars:
+            names.append(c["Identification"]["Make"])
+        options += Markup("<option value=\"" + d["Identification"]["Make"] + "\">" + d["Identification"]["Make"] + "</option>")
+        
 def get_longest_car(cars)
     length = 0
     longestCar = ""

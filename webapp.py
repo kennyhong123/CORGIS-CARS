21e846b934d2b7fae1efc5e9710d720aa8be5595
kennyhong123 <31251440+kennyhong123@users.noreply.github.com>
2018-01-15 18:52:19 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -22,7 +22,7 @@ def render_main():
 def render_largestCars():
     return render_template('largest-cars.html')
     
-@app.route("/mpgPerCars")
+@app.route("/mpgPerCars", methods=['GET','POST'])
 def render_mpg_per_cars():
     return render_template('mpg-per-car.html', option = get_car_options())
 

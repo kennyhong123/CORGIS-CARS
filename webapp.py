bbd14a395128aa38d019a206e140dcf5a3bac9f8
kennyhong123 <31251440+kennyhong123@users.noreply.github.com>
2018-01-15 18:40:55 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -4,6 +4,15 @@ import json
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
+with open('car.json') as car_data:
+    data = json.load(car_data)
+
+def get_car_names():
+    options = ""
+    for car in data:
+        options += Markup("option value=" + '""' + car["Identification"]["ID"] + '">' + car["Identification"]["ID"] + "</option>")
+    return options
+
 @app.route("/")
 def render_main():
     return render_template('home.html')
@@ -15,6 +24,10 @@ def render_largestCars():
 @app.route("/mpgPerCars")
 def render_mpg_per_cars():
     return render_template('mpg-per-car.html')
+
+def get_mpg_per_car(cars):
+    
+
 
 if __name__=="__main__":
     app.run(debug=False, port=54321)

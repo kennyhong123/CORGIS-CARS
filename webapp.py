7b306fe5998f6730000beceda80e21ff086543be
kennyhong123 <31251440+kennyhong123@users.noreply.github.com>
2018-01-15 18:14:32 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -9,38 +9,8 @@ def render_main():
     return render_template('home.html')
 
 @app.route("/largestCars")
-def render_largest_cars():
-  with open('cars.json') as cars_data:
-        cars = json.load(cars_data)
-    longestData = get_longest_car(cars)
-    tallestData = get_tallest_car(cars)
-    return render_template('largest-cars.html',longest = longestData[0], length = longestData[1], tallest = tallestData[0], height = tallestData[1])
-
-def get_car_options(cars):
-    names = []
-    options = ""
-    for c in cars:
-        if c["Identification"]["Make"] not in cars:
-            names.append(c["Identification"]["Make"])
-        options += Markup("<option value=\"" + c["Identification"]["Make"] + "\">" + c["Identification"]["Make"] + "</option>")
-        
-def get_longest_car(cars)
-    length = 0
-    longestCar = ""
-    for c in cars:
-        if c["Dimensions"]["Length"] > length:
-            length = c["Dimensions"]["Length"]
-            longestCar = c["Identification"]["Make"]
-    return [longestCar, length]
-      
-def get_tallest_car(cars):
-    height = 0
-    tallestCar = ""
-    for c in cars:
-        if c["Dimensions"]["Height"] > height:
-            height = c["Dimensions"]["Height"]
-            tallestCar = c["Identification"]["Make"]
-    return [tallestCar, height]
+def render_largestCars():
+    return render_template('largest-cars.html')
     
 if __name__=="__main__":
     app.run(debug=False, port=54321)
